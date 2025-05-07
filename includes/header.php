cc975da9f768ea16e9b9c9ceb4abedd9d5c4b07a
Rexie N. Villanueva <rexievillanueva03@gmail.com>
2025-05-07 21:45:48 +0800

    fixing layout

--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,35 @@
-<Header class="header p-5 bg-gray-800 text-white text-center">
+<Header class="header p-3 text-white body-teritiary">
     <div class="container mx-auto">
-        <nav class="flex justify-between items-center">
-            <div class="w-5 flex-1 text-left">Logo</div>
-            <div class="w-95 flex-2 text-right">
-                
+        <h3 class="fw-bold text-center"><span  class="text-warning">BUZZER</span>Beater</h3>
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <div class="d-flex justify-content-end text-center">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
-    
+    </div>
 </Header>
--- public/index.php
+++ public/index.php
@@ -6,17 +6,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Online Gadget Store</title>
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.12.1/font/bootstrap-icons.min.css">
 </head>
 <body>
     <?php include '../includes/header.php'; ?>
 
     <main class="h-screen w-screen bg-gray-100 flex items-center justify-center">
-        <div class="bg-white shadow-md rounded-lg p-8 max-w-md w-full">
-            <h2 class="text-2xl font-bold mb-6 text-center">Welcome to Online Gadget Store</h2>
-            <p class="text-gray-700 mb-4">Discover the latest gadgets and electronics at unbeatable prices.</p>
-            <a href="products.php" class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">Shop Now</a>
-        </div>
+        
     </main>
 
     <?php include '../includes/footer.php'; ?>
